7784ad9a2f068b95837babe1e4654cb30b002c17
Naardennj <gabriel.angelis787@gmail.com>
2025-02-04 10:40:37 -0300

    Refatorar o projeto modificando a estratégia de recarregamento Utilizar o BehaviorSubject para gerenciar o estado da aplicação

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -182,36 +182,40 @@ export const flyInOutTrigger = trigger('flyInOut', [
 
 export const shakeTrigger = trigger('shakeAnimation', [
   transition('* => *', [
-    query('input.ng-invalid:focus, select.ng-invalid:focus', [
-      animate(
-        '0.5s',
-        keyframes([
-          style({
-            border: '2px solid red',
-          }),
-          style({
-            transform: 'translateX(-10px)',
-          }),
-          style({
-            transform: 'translateX(10px)',
-          }),
-          style({
-            transform: 'translateX(-10px)',
-          }),
-          style({
-            transform: 'translateX(10px)',
-          }),
-          style({
-            transform: 'translateX(-10px)',
-          }),
-          style({
-            transform: 'translateX(10px)',
-          }),
-          style({
-            transform: 'translateX(0px)',
-          }),
-        ])
-      ),
-    ], {optional: true}),
+    query(
+      'input.ng-invalid:focus, select.ng-invalid:focus',
+      [
+        animate(
+          '0.5s',
+          keyframes([
+            style({
+              border: '2px solid red',
+            }),
+            style({
+              transform: 'translateX(-10px)',
+            }),
+            style({
+              transform: 'translateX(10px)',
+            }),
+            style({
+              transform: 'translateX(-10px)',
+            }),
+            style({
+              transform: 'translateX(10px)',
+            }),
+            style({
+              transform: 'translateX(-10px)',
+            }),
+            style({
+              transform: 'translateX(10px)',
+            }),
+            style({
+              transform: 'translateX(0px)',
+            }),
+          ])
+        ),
+      ],
+      { optional: true }
+    ),
   ]),
 ]);
